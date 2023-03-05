f3794cdf0025bdde076b430bccf4326e35e594aa
Nil Patel <nilrnp@gmail.com>
2023-03-05 00:59:20 -0800

    first commit

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+# main function (ask for user input here)
+def main():
+    return
+
+# perform addition
+def addition(num1, num2):
+    return
+
+# perform subtraction
+def subtraction(num1,num2):
+    return
+
+# perform multiplication
+def multiplication(num1, num2):
+    return
+
+# perform division
+def division(num1,num2):
+    return
+
+# check if entered string is a valid decimal integer
+def checkIfDecimal():
+    return
+
+if __name__ == '__main__':
+    main()
