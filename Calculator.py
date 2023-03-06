1166546bcdfcd3875b7f22bf3a631229455333b3
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2023-03-05 21:30:54 -0800

    added functions to get the hex numbers

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,9 @@
 # main function (ask for user input here)
 def main():
+    string = input("What hex number would you like to turn into a decimal number?")
+    tring = input("What octal number would you like to turn into a decimal number?")
+    print(getHexNumber(string))
+    print(checkIfOctal(tring))
     return
 
 # perform addition
@@ -19,8 +23,20 @@ def division(num1,num2):
     return
 
 # check if entered string is a valid decimal integer
-def checkIfDecimal():
+def checkIfDecimal(string):
     return
+
+def checkIfOctal(string):
+    return
+
+def checkIfHex(string):
+    return
+
+def getOctalNumber(string):
+    return int(string,8)
+
+def getHexNumber(string):
+    return int(string,16)
 
 if __name__ == '__main__':
     main()
