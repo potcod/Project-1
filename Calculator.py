122f56352d5c17b1ecceffdcda6231c3a717ac3d
Nil Patel <nilrnp@gmail.com>
2023-03-08 14:04:47 -0800

    added function to get operation type

--- Calculator.py
+++ Calculator.py
@@ -13,31 +13,46 @@ def main():
         num1InDecimal = getHexNumber(num1)
         num2InDecimal = getHexNumber(num2)
     else:
-        num1InDecimal = num1
-        num2InDecimal = num2
+        num1InDecimal = int(num1)
+        num2InDecimal = int(num2)
 
+    operationType = getOperation()
 
     return
+
 
 # perform addition
 def addition(num1, num2):
-    return num1 + num2
+    return (num1 + num2)
+
 
 # perform subtraction
 def subtraction(num1, num2):
-    return num1 - num2
+    return (num1 - num2)
+
 
 # perform multiplication
 def multiplication(num1, num2):
-    return num1 * num2
+    return (num1 * num2)
+
 
 # perform division
 def division(num1, num2):
-    return num1 / num2
+    return (num1 / num2)
+
 
 # get what type of operation will be done on the numbers
 def getOperation():
-    return
+    print("What type of operation would you like to do?\n1: Addition\n2: Subtraction\n3: Multiplication\n4: Division")
+    operationType = int(input("Operation type: "))
+    while True:
+        if (operationType == 1) or (operationType == 2) or (operationType == 3) or (operationType == 4):
+            return operationType
+        else:
+            print(
+                "Please select a valid option.\n1: Addition\n2: Subtraction\n3: Multiplication\n4: Division")
+            operationType = int(input("Operation type: "))
+
 
 # get what type of number is being used for calculations
 def getNumberType():
@@ -49,6 +64,7 @@ def getNumberType():
         else:
             print("Please select a valid option.\n1: Decimal\n2: Octal\n3: Hexadecimal")
             type = int(input("Number type: "))
+
 
 # get input numbers based on the number type
 def getInputs(numberType):
@@ -63,6 +79,7 @@ def getInputs(numberType):
         checkIfHex(numString)
     return numString
 
+
 # check if entered string is a valid decimal integer
 def checkIfDecimal(num_str):
     try:
@@ -72,7 +89,8 @@ def checkIfDecimal(num_str):
         print("Please enter a valid decimal number")
         exit()
 
-# check if entered string is a valid octal integer    
+
+# check if entered string is a valid octal integer
 def checkIfOctal(num_str):
     try:
         int(num_str, 8)
@@ -80,6 +98,7 @@ def checkIfOctal(num_str):
     except ValueError:
         print("Please enter a valid octal number")
         exit()
+
 
 # check if entered string is a valid hexadecimal integer
 def checkIfHex(num_str):
@@ -90,13 +109,16 @@ def checkIfHex(num_str):
         print("Please enter a valid hexadecimal number")
         exit()
 
+
 # converts the octal number to decimal
 def getOctalNumber(string):
     return int(string, 8)
 
+
 # converts the hexadecimal number to decimal
 def getHexNumber(string):
     return int(string, 16)
+
 
 if __name__ == '__main__':
     main()
