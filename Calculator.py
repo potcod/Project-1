c288bc1a82fce445da382cd5dd5adc94b3fe78a7
Nil Patel <nilrnp@gmail.com>
2023-03-08 13:17:46 -0800

    added conversion functionality

--- Calculator.py
+++ Calculator.py
@@ -1,26 +1,43 @@
 # main function (ask for user input here)
 def main():
     numberType = getNumberType()
-    number1 = getInputs(numberType)
-    number2 = getInputs(numberType)
+    print("Enter first number")
+    num1 = getInputs(numberType)
+    print("Enter second number")
+    num2 = getInputs(numberType)
+
+    if (numberType == 2):
+        num1InDecimal = getOctalNumber(num1)
+        num2InDecimal = getOctalNumber(num2)
+    elif (numberType == 3):
+        num1InDecimal = getHexNumber(num1)
+        num2InDecimal = getHexNumber(num2)
+    else:
+        num1InDecimal = num1
+        num2InDecimal = num2
+
 
     return
 
 # perform addition
 def addition(num1, num2):
-    return
+    return num1 + num2
 
 # perform subtraction
-def subtraction(num1,num2):
-    return
+def subtraction(num1, num2):
+    return num1 - num2
 
 # perform multiplication
 def multiplication(num1, num2):
-    return
+    return num1 * num2
 
 # perform division
-def division(num1,num2):
-    return
+def division(num1, num2):
+    return num1 / num2
+
+# get what type of operation will be done on the numbers
+def getOperation():
+    return 
 
 # get what type of number is being used for calculations
 def getNumberType():
@@ -36,13 +53,13 @@ def getNumberType():
 # get input numbers based on the number type
 def getInputs(numberType):
     if numberType == 1:
-        numString = str(input("Enter number: "))
+        numString = str(input(""))
         checkIfDecimal(numString)
     elif numberType == 2:
-        numString = str(input("Enter number: "))
+        numString = str(input(""))
         checkIfOctal(numString)
     elif numberType == 3:
-        numString = str(input("Enter number: "))
+        numString = str(input(""))
         checkIfHex(numString)
     return numString
 
@@ -52,16 +69,16 @@ def checkIfDecimal(num_str):
         int(num_str)
         return True
     except ValueError:
-        print("Enter a valid decimal number")
+        print("Please enter a valid decimal number")
         exit()
-    
+
 # check if entered string is a valid octal integer    
 def checkIfOctal(num_str):
     try:
         int(num_str, 8)
         return True
     except ValueError:
-        print("Enter a valid octal number")
+        print("Please enter a valid octal number")
         exit()
 
 # check if entered string is a valid hexadecimal integer
@@ -70,16 +87,16 @@ def checkIfHex(num_str):
         int(num_str, 16)
         return True
     except ValueError:
-        print("Enter a valid hexadecimal number")
+        print("Please enter a valid hexadecimal number")
         exit()
 
-#converts the octal number to decimal
+# converts the octal number to decimal
 def getOctalNumber(string):
-    return int(string,8)
+    return int(string, 8)
 
-#converts the hexadecimal number to decimal
+# converts the hexadecimal number to decimal
 def getHexNumber(string):
-    return int(string,16)
+    return int(string, 16)
 
 if __name__ == '__main__':
     main()
