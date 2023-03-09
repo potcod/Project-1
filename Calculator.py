6726a3a0fb0f6f148e991396a51c6d6b31ff4b3c
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2023-03-09 15:00:40 -0800

    added calculations with printing answer

--- Calculator.py
+++ Calculator.py
@@ -17,28 +17,29 @@ def main():
         num2InDecimal = int(num2)
 
     operationType = getOperation()
+    performOperation(num1InDecimal,num2InDecimal, operationType)
 
     return
 
 
 # perform addition
 def addition(num1, num2):
-    return (num1 + num2)
+    return print("The two numbers added together =", num1 + num2)
 
 
 # perform subtraction
 def subtraction(num1, num2):
-    return (num1 - num2)
+    return print("The two numbers subtracted =", num1 - num2)
 
 
 # perform multiplication
 def multiplication(num1, num2):
-    return (num1 * num2)
+    return print("The two numbers multiplied together =", num1 * num2)
 
 
 # perform division
 def division(num1, num2):
-    return (num1 / num2)
+    return print("The two numbers divided =", num1 / num2)
 
 
 # get what type of operation will be done on the numbers
@@ -53,6 +54,17 @@ def getOperation():
                 "Please select a valid option.\n1: Addition\n2: Subtraction\n3: Multiplication\n4: Division")
             operationType = int(input("Operation type: "))
 
+#performs the operation on the two numbers
+def performOperation(num1,num2,operationNumber):
+    if(operationNumber == 1):
+        addition(num1,num2)
+    elif(operationNumber == 2):
+        subtraction((num1,num2))
+    elif(operationNumber == 3):
+        multiplication(num1,num2)
+    else:
+        division(num1,num2)
+    
 
 # get what type of number is being used for calculations
 def getNumberType():
